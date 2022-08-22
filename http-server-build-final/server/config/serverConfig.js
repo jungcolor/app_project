3ef0a423353d24d8c3765f014a390334cad141e4
jungcolor <jungcolor@gmail.com>
2022-08-23 00:28:54 +0900

    ssl key 설치 및 https적용

--- http-server-build-final/server/config/serverConfig.js
+++ http-server-build-final/server/config/serverConfig.js
@@ -1,4 +1,6 @@
-const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 const defaultPort = 3030;
 
 module.exports = {
@@ -10,9 +12,16 @@ module.exports = {
     },
 
     createServer: function () {
-        http.createServer(this.callback)
+        https.createServer(this.getSSLOptions(), this.callback)
             .listen(this.port, () => {
                 console.log(`[Server] listening on port ${this.port}`);
             });
+    },
+
+    getSSLOptions: function () {
+        return {
+            key: fs.readFileSync(path.resolve("./server/ssl/private.pem")),
+            cert: fs.readFileSync(path.resolve("./server/ssl/public.pem"))
+        }
     }
 }
