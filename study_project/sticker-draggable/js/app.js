0ca1a1c406d38be2177725c43df16d2f38e7f536
jungcolor <jungcolor@gmail.com>
2022-10-09 10:56:36 +0900

    sticker 리팩토링 작업

--- study_project/sticker-draggable/js/app.js
+++ study_project/sticker-draggable/js/app.js
@@ -3,16 +3,16 @@ import StickerLayout from "./stickerLayout.js";
 window.addEventListener("DOMContentLoaded", (e) => {
     const stickerAdd = document.querySelector("#sticker-add");
     const stickerRemoveAll = document.querySelector("#sticker-remove-all");
-    const stickerLayout = new StickerLayout();
-    stickerLayout.render(document.body);
+    // const stickerLayout = new StickerLayout();
+    // stickerLayout.render(document.body);
 
-    // 스티커 추가
-    stickerAdd.addEventListener("click", e => {
-        stickerLayout.createSticker();
-    });
+    // // 스티커 추가
+    // stickerAdd.addEventListener("click", e => {
+    //     stickerLayout.createSticker();
+    // });
 
-    // 스티커 전부 삭제
-    stickerRemoveAll.addEventListener("click", e => {
-        stickerLayout.removeStickerAll();
-    });
+    // // 스티커 전부 삭제
+    // stickerRemoveAll.addEventListener("click", e => {
+    //     stickerLayout.removeStickerAll();
+    // });
 });
