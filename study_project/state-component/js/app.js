452196a435409a24d8510b87753c917730b179cd
jungcolor <jungcolor@gmail.com>
2022-10-14 00:47:50 +0900

    DOM을 직접 다룰필요가 없는 코드 DOM > state에 종속됨

--- study_project/state-component/js/app.js
+++ study_project/state-component/js/app.js
@@ -1,3 +1,7 @@
+// state가 변경 되면 render를 실행한다
+// state는 setState로만 변경해야 한다
+// 위 규칙을 지켜가면서 코드를 작성하게 되면 브라우저에 출력되는 내용은 무조건 state에 종속되는 것이다.
+// DOM을 직접적으로 다룰 필요가 없어진다
 const $app = document.querySelector("#app");
 
 let state = {
