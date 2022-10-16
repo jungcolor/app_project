cfa76165f804bf584d194a178d683d1f87c9c99d
jungcolor <jungcolor@gmail.com>
2022-10-17 00:34:51 +0900

    재사용을 위해 함수로 변경

--- study_project/state-component/src/observer.js
+++ study_project/state-component/src/observer.js
@@ -48,43 +48,61 @@
 
 // 상태.내부에_변화가_생김({ a: 100, b: 200 });
 
+// const 덧셈_계산기 = () => {
+//     currentObserver = 덧셈_계산기;
+//     console.log(`a + b = ${state.a + state.b}`);
+// };
+
+// const 뺄셈_계산기 = () => {
+//     currentObserver = 뺄셈_계산기;
+//     console.log(`a - b = ${state.a - state.b}`);
+// }
+
+// 덧셈_계산기();
+// state.a = 100;
+
+// 뺄셈_계산기();
+// state.b = 200;
+
+// state.a = 1;
+// state.b = 2;
+
 // 리팩토링
 let currentObserver = null;
 
-const state = { a: 10, b: 20 };
-const stateKeys = Object.keys(state);
+const observe = fn => {
+    currentObserver = fn;
+    fn();
+    currentObserver = null;
+}
 
-for (const key of stateKeys) {
-    let _value = state[key];
-    const observers = new Set();
-    Object.defineProperty(state, key, {
-        get () {
-            if (currentObserver) observers.add(currentObserver);
-            return _value;
-        },
+const observable = obj => {
+    Object.keys(obj).forEach(key => {
+        let _value = obj[key];
+        const observers = new Set();
 
-        set (value) {
-            _value = value;
-            observers.forEach(observer => observer());
-        }
+        Object.defineProperty(obj, key, {
+            get() {
+                if (currentObserver) observers.add(currentObserver);
+                return _value;
+            },
+
+            set(value) {
+                _value = value;
+                observers.forEach(observer => observer());
+            }
+        });
     });
+
+    return obj;
 }
 
-const 덧셈_계산기 = () => {
-    currentObserver = 덧셈_계산기;
-    console.log(`a + b = ${state.a + state.b}`);
-};
+const 상태 = observable({ a: 10, b: 20 });
+observe(() => console.log(`a = ${상태.a}`));
+observe(() => console.log(`b = ${상태.b}`));
+observe(() => console.log(`a + b = ${상태.a} + ${상태.b}`));
+observe(() => console.log(`a * b = ${상태.a} * ${상태.b}`));
+observe(() => console.log(`a - b = ${상태.a} - ${상태.b}`));
 
-const 뺄셈_계산기 = () => {
-    currentObserver = 뺄셈_계산기;
-    console.log(`a - b = ${state.a - state.b}`);
-}
-
-덧셈_계산기();
-state.a = 100;
-
-뺄셈_계산기();
-state.b = 200;
-
-state.a = 1;
-state.b = 2;
+상태.a = 100;
+상태.b = 200;
