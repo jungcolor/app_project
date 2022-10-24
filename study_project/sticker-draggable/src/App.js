2707846c07d1e59dd28b7d99c4481b52d7f3b562
jungcolor <jungcolor@gmail.com>
2022-10-24 20:59:34 +0900

    컴포넌트로 스티커 생성까지 완료

--- study_project/sticker-draggable/src/App.js
+++ study_project/sticker-draggable/src/App.js
@@ -1,23 +1,75 @@
 import Component from "./core/Component.js";
-import StickerContainer from "./stickerContainer.js";
+// import StickerContainer from "./components/StickerContainer.js";
+import Sticker from "./components/Sticker.js";
 
 export default class App extends Component {
-    
+    setup() {
+        this.state = { items: [] };
+    }
+
+    template() {
+        return `
+            <button type="button" class="btn" id="sticker-add">스티커 추가</button>
+            <button type="button" class="btn" id="sticker-remove-all">스티커 모두 삭제</button>
+            <div class="sticker-wrapper"></div>
+        `;
+    }
+
+    // 하위 컴포넌트 생성
+    mounted() {
+        const { $target, stickers, removeSticker } = this;
+        const $stickerWrapper = $target.querySelector(".sticker-wrapper");
+
+        new Sticker($stickerWrapper, { stickers, removeSticker: removeSticker.bind(this) });
+    }
+
+    setEvent() {
+        this.addEvent("click", "#sticker-add", ({ target }) => {
+            this.addSticker();
+        });
+
+        this.addEvent("click", "#sticker-remove-all", ({ target }) => {
+            console.log("스티커 삭제");
+        });
+    }
+
+    get stickers() {
+        return this.state.items;
+    }
+
+    addSticker() {
+        const result = [{
+            id: crypto.randomUUID(),
+            style: {
+                position: { top: 10, left: 10 },
+                bgColor: "#fff",
+            },
+            children: []
+        }];
+        this.setState(result);
+    }
+
+    removeSticker(id) {
+        const stickers = [...this.state];
+        stickers.splice(stickers.filter(sticker => sticker.id === id), 1);
+        this.setState(stickers);
+    }
 }
 
-window.addEventListener("DOMContentLoaded", (e) => {
-    const stickerAdd = document.querySelector("#sticker-add");
-    const stickerRemoveAll = document.querySelector("#sticker-remove-all");
-    const stickerContainer = new StickerContainer();
-    stickerContainer.render(document.body);
 
-    // 스티커 추가
-    stickerAdd.addEventListener("click", e => {
-        stickerContainer.createSticker();
-    });
+// window.addEventListener("DOMContentLoaded", (e) => {
+//     const stickerAdd = document.querySelector("#sticker-add");
+//     const stickerRemoveAll = document.querySelector("#sticker-remove-all");
+//     const stickerContainer = new StickerContainer();
+//     stickerContainer.render(document.body);
 
-    // 스티커 전부 삭제
-    stickerRemoveAll.addEventListener("click", e => {
-        stickerContainer.removeStickerAll();
-    });
-});
+//     // 스티커 추가
+//     stickerAdd.addEventListener("click", e => {
+//         stickerContainer.createSticker();
+//     });
+
+//     // 스티커 전부 삭제
+//     stickerRemoveAll.addEventListener("click", e => {
+//         stickerContainer.removeStickerAll();
+//     });
+// });
--- study_project/sticker-draggable/src/core/Component.js
+++ study_project/sticker-draggable/src/core/Component.js
@@ -1,42 +1,43 @@
 export default class Component {
-    target;
-    state;
+    $target;
     props;
+    state;
 
-    constructor(target, props) {
-        this.target = target; // parent
-        this.props = props;
+    constructor($target, props) {
+        this.$target = $target;
+        this.props = props
         this.setup();
-        this.render();
         this.setEvent();
+        this.render();
     }
 
     setup() { }
 
     setEvent() { }
-    
+
+    template() { return ""; }
+
+    mounted() { }
+
     render() {
-        this.target.innerHTML = this.template();
+        this.$target.innerHTML = this.template();
         this.mounted();
     }
 
-    template() { return `` }
-    
-    mounted() {}
-
-    setState(newState) {
-        this.state = { ...this.state, ...newState };
-        this.render();
-    }
-
+    // event 추가 - 이벤트 버블링 추상화
     addEvent(eventType, selector, callback) {
-        const children = [...this.target.querySelectorAll(selector)];
+        const children = [this.$target.querySelectorAll(selector)];
         const isTarget = (target) => children.includes(target) || target.closest(selector);
 
-        this.target.addEventListener(eventType, event => {
+        this.$target.addEventListener(eventType, event => {
             if (!isTarget(event.target)) return false;
 
             callback(event);
         });
+    }
+
+    setState(newState) {
+        this.state = [...this.state, ...newState];
+        this.render();
     }
 }
