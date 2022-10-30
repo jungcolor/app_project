9d77dc6ea57be501d0c4fd4eb2ede5649b5f3818
jungcolor <jungcolor@gmail.com>
2022-10-30 17:54:18 +0900

    webpack 적용

---
+++ study_project/restaurant-operation/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./js/main.js",
+    mode: "development",
+    output: {
+        filename: "[name].[id].[hash].bundle.js",
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "./index.html",
+            chunks: ["index"]
+        })
+    ]
+}
