5d764b2467cdf98f156fb81751509199323332e7
jungcolor <jungcolor@gmail.com>
2022-12-13 00:26:40 +0900

    html 태그 구조 추가

--- study_project/react-news-viewer/src/components/App.tsx
+++ study_project/react-news-viewer/src/components/App.tsx
@@ -3,9 +3,29 @@ import React from "react";
 function App() {
     return (
         <div className="App">
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
+            <header>
+                <nav>
+                    <a href="#">전체보기</a>
+                    <a href="#">비즈니스</a>
+                    <a href="#">엔터테인먼트</a>
+                    <a href="#">건강</a>
+                    <a href="#">과학</a>
+                    <a href="#">스포츠</a>
+                    <a href="#">기술</a>
+                </nav>
+            </header>
+            <div className="contents">
+                <article className="viewer-list">
+                    <h3>뉴스기사</h3>
+                    <a href="#">
+                        <figure>이미지</figure>
+                        <dl>
+                            <dt>타이틀</dt>
+                            <dd>내용</dd>
+                        </dl>
+                    </a>
+                </article>
+            </div>
         </div>
     )
 }
