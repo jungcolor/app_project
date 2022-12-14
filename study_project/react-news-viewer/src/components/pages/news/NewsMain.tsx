8780ce1891edafeae7f85126004a5bd51edbb641
jungcolor <jungcolor@gmail.com>
2022-12-15 00:19:56 +0900

    api data success

--- study_project/react-news-viewer/src/components/pages/news/NewsMain.tsx
+++ study_project/react-news-viewer/src/components/pages/news/NewsMain.tsx
@@ -1,6 +1,22 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { createNoSubstitutionTemplateLiteral } from "typescript";
+import { apiKey } from "../../../api/types";
 
 const NewsMain = () => {
+    const [data, setData] = useState([]);
+    const fetchData = async () => {
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${apiKey}`);
+        setData(response.data.articles);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    console.log(data);
+
+
     return <div>메인 & 전체보기</div>;
 };
 
