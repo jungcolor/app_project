ab981005d512f7d2d2cc2c1785a00a47d0fa0f49
jungcolor <jungcolor@gmail.com>
2022-12-26 19:05:41 +0900

    code splitting 적용

--- study_project/react-editor/src/App.tsx
+++ study_project/react-editor/src/App.tsx
@@ -1,31 +1,32 @@
 // lib
-import React, { useEffect } from "react";
+import React, { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
 // components
-import PostList from "./components/posts/PostList";
-import PostView from "./components/posts/PostView";
-import PostWrite from "./components/posts/PostWrite";
 import { useAppSelector } from "./app/hooks";
 
 // css
 import "./App.css";
 import Header from "./components/layout/Header";
-import { IData } from "./interface/Post.interface";
 
 const App = () => {
     // 초기값 가져오는 함수
     const posts = useAppSelector((state) => state.post);
+    const PostList = lazy(() => import("./components/posts/PostList"));
+    const PostWrite = lazy(() => import('./components/posts/PostWrite'));
+    const PostView = lazy(() => import("./components/posts/PostView"));
 
     return (
         <div className="app">
             <Header />
             <main className="contents">
-                <Routes>
-                    <Route path="/" element={<PostList items={posts} />}></Route>
-                    <Route path="/postWrite" element={<PostWrite />}></Route>
-                    <Route path="/postView/:title" element={<PostView viewDatas={posts} />}></Route>
-                </Routes>
+                <Suspense fallback={<div>Loading....</div>}>
+                    <Routes>
+                        <Route path="/" element={<PostList items={posts} />}></Route>
+                        <Route path="/postWrite" element={<PostWrite />}></Route>
+                        <Route path="/postView/:title" element={<PostView viewDatas={posts} />}></Route>
+                    </Routes>
+                </Suspense>
             </main>
         </div>
     );
