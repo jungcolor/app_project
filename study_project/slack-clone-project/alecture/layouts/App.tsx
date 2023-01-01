769c45b6d44f65ac60012b072ec0fd60ff32e399
jungcolor <jungcolor@gmail.com>
2023-01-01 16:50:25 +0900

    코드 스플리팅 + html, css(이모션) 설정

--- study_project/slack-clone-project/alecture/layouts/App.tsx
+++ study_project/slack-clone-project/alecture/layouts/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import loadable from '@loadable/component';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import Login from '@pages/Login';
-import SignUp from '@pages/SignUp';
+
+const LogIn = loadable(() => import('@pages/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp'));
 
 const App = () => {
     return (
         <Switch>
             <Redirect exact path="/" to="/login" />
-            <Route path="/login" component={Login} />
+            <Route path="/login" component={LogIn} />
             <Route path="/signup" component={SignUp} />
         </Switch>
     );
