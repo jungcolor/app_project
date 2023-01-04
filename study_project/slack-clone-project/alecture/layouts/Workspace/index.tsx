298a2a071e99102478685b2efb52cbdbb8bfc1ba
jungcolor <jungcolor@gmail.com>
2023-01-04 22:25:51 +0900

    menu layer 추가

--- study_project/slack-clone-project/alecture/layouts/Workspace/index.tsx
+++ study_project/slack-clone-project/alecture/layouts/Workspace/index.tsx
@@ -3,14 +3,16 @@ import axios from 'axios';
 import gravatar from 'gravatar';
 import fetcher from '@utils/fetcher';
 import loadable from '@loadable/component';
-import React, { FC, useCallback } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 import { Redirect, Route, Switch } from 'react-router';
-import { Channels, Chats, Header, MenuScroll, ProfileImg, RightMenu, WorkspaceName, Workspaces, WorkspaceWrapper } from '@layouts/Workspace/styles';
+import { Channels, Chats, Header, LogOutButton, MenuScroll, ProfileImg, ProfileModal, RightMenu, WorkspaceName, Workspaces, WorkspaceWrapper } from '@layouts/Workspace/styles';
+import Menu from '@components/Menu';
 
 const Channel = loadable(() => import('@pages/Channel'));
 const DirectMessage = loadable(() => import('@pages/DirectMessage'));
 
 const Workspace: FC = ({ children }) => {
+    const [showUserMenu, setShowUserMenu] = useState(false);
     const { data, error, mutate } = useSWR('http://localhost:3095/api/users', fetcher, {
         dedupingInterval: 2000, // 2초
     });
@@ -24,6 +26,10 @@ const Workspace: FC = ({ children }) => {
             });
     }, []);
 
+    const onClickUserProfile = useCallback(() => {
+        setShowUserMenu((prev) => !prev);
+    }, []);
+
     if (!data) {
         return <Redirect to="/login" />;
     }
@@ -32,8 +38,20 @@ const Workspace: FC = ({ children }) => {
         <div>
             <Header>
                 <RightMenu>
-                    <span>
+                    <span onClick={onClickUserProfile}>
                         <ProfileImg src={gravatar.url(data.nickname, { s: "28px", d: "retro" })} alt={data.nickname} />
+                        {showUserMenu &&
+                            <Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseModal={onClickUserProfile}>
+                                <ProfileModal>
+                                    <img src={gravatar.url(data.nickname, { s: "36px", d: "retro" })} alt={data.nickname} />
+                                    <div>
+                                        <span id="profile-name">{data.nickname}</span>
+                                        <span id="profile-active">Active</span>
+                                    </div>
+                                </ProfileModal>
+                                <LogOutButton onClick={onLogout}>로그아웃</LogOutButton>
+                            </Menu>
+                        }
                     </span>
                 </RightMenu>
             </Header>
