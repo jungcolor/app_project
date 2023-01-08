bf7bba7657efac180ca3d5d7b76d755201befb46
jungcolor <jungcolor@gmail.com>
2023-01-08 15:18:22 +0900

    채팅 보내기 및 eslint 추가 설정 적용

---
+++ study_project/slack-clone-project/alecture/components/ChatList/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ChatZone } from '@components/ChatList/styles';
+
+const ChatList = () => {
+    return (
+        <ChatZone>
+
+        </ChatZone>
+    );
+};
+
+export default ChatList;
--- study_project/slack-clone-project/alecture/pages/DirectMessage/index.tsx
+++ study_project/slack-clone-project/alecture/pages/DirectMessage/index.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Container, Header } from '@pages/DirectMessage/styles';
 import gravatar from 'gravatar';
 import useSWR from 'swr';
 import fetcher from '@utils/fetcher';
 import { useParams } from 'react-router';
+import useInput from '@hooks/useInput';
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+import axios from 'axios';
+import { IDM } from '@typings/db';
 
 const DirectMessage = () => {
     const { workspace, id } = useParams<{ workspace: string, id: string }>();
     const { data: userData } = useSWR(`http://localhost:3095/api/workspaces/${workspace}/users/${id}`, fetcher);
     const { data: myData } = useSWR(`http://localhost:3095/api/users`, fetcher);
+    const [chat, onChangeChat, setChat] = useInput('');
+    const { data: chatData, mutate: chatMutate } = useSWR<IDM[]>(`http://localhost:3095/api/workspace/${workspace}/dms/${id}/chats?perPage=20&page=1`, fetcher);
+
+    const onSubmitForm = useCallback((e) => {
+        e.preventDefault();
+
+        if (chat?.trim()) {
+            axios.post(`http://localhost:3095/api/workspace/${workspace}/dms/${id}/chats`, {
+                content: chat,
+            })
+                .then(() => {
+                    chatMutate();
+                    setChat('');
+                })
+                .catch(console.error)
+        }
+    }, [chat]);
 
     if (!userData || !myData) return null;
 
@@ -18,8 +40,8 @@ const DirectMessage = () => {
                 <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
                 <span>{userData.nickname}</span>
             </Header>
-            {/* <ChatList /> */}
-            {/* <ChatBox /> */}
+            <ChatList />
+            <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
         </Container>
     );
 };
