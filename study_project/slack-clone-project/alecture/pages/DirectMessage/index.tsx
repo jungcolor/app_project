426a2399f6206cf389fd3ffcc246bdf61c4407c3
jungcolor <jungcolor@gmail.com>
2023-01-08 14:34:03 +0900

    ChatBox, ChatList 추가

--- study_project/slack-clone-project/alecture/pages/DirectMessage/index.tsx
+++ study_project/slack-clone-project/alecture/pages/DirectMessage/index.tsx
@@ -1,9 +1,26 @@
-import Workspace from '@layouts/Workspace';
 import React from 'react';
+import { Container, Header } from '@pages/DirectMessage/styles';
+import gravatar from 'gravatar';
+import useSWR from 'swr';
+import fetcher from '@utils/fetcher';
+import { useParams } from 'react-router';
 
 const DirectMessage = () => {
+    const { workspace, id } = useParams<{ workspace: string, id: string }>();
+    const { data: userData } = useSWR(`http://localhost:3095/api/workspaces/${workspace}/users/${id}`, fetcher);
+    const { data: myData } = useSWR(`http://localhost:3095/api/users`, fetcher);
+
+    if (!userData || !myData) return null;
+
     return (
-        <div>메세지~~~~~~</div>
+        <Container>
+            <Header>
+                <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+                <span>{userData.nickname}</span>
+            </Header>
+            {/* <ChatList /> */}
+            {/* <ChatBox /> */}
+        </Container>
     );
 };
 
